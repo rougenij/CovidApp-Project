e2da31968fe08ed52d3166060cd51663c6874527
Rouge Nij <rougenij@gmail.com>
2021-12-09 13:13:40 +0200

    Creating files for project

---
+++ Scripts/covid.js
@@ -0,0 +1,13 @@
+//* Psuedo code *\\
+//* Create an object that has 5 keys
+//fetch('https://corona-api.com/countries').then((response) => response.json()).then((res)=>console.log(res)); --> Gets the Data of every country
+//fetch('https://intense-mesa-62220.herokuapp.com/https://restcountries.herokuapp.com/api/v1/region/${name]').then((response) => response.json()).then((res)=> console.log(res));
+// const continentObj = {
+//     asia: [],
+//     europe: [],
+//     africa: [],
+//     america:[],
+// }
+const SRC = "https://corona-api.com/countries";
+const secondSRC = `${proxy}https://restcountries.herokuapp.com/api/v1/region/${name]``;
+const proxy = "https://intense-mesa-62220.herokuapp.com/";
