32e9b3df015c88e02bacc9306813358f788c1ecf
Rouge Nij <rougenij@gmail.com>
2021-12-09 15:09:06 +0200

    Added Chart Library

---
+++ Scripts/psuedo code.js
@@ -0,0 +1,13 @@
+//* Psuedo code *\\
+//* Create an object that has 5 keys
+//fetch('https://corona-api.com/countries').then((response) => response.json()).then((res)=>console.log(res)); --> Gets the Data of every country
+//fetch('https://intense-mesa-62220.herokuapp.com/https://restcountries.herokuapp.com/api/v1/region/${name]').then((response) => response.json()).then((res)=> console.log(res));
+//! This Object starts empty
+// const continentObj = {}
+// const SRC = "https://corona-api.com/countries";
+// const secondSRC = `${proxy}https://restcountries.herokuapp.com/api/v1/region/${name]`;
+// const proxy = "https://intense-mesa-62220.herokuapp.com/";
+
+//Empty Screen --> empty Graph
+// fillContinentObj --> first we fetch the data from the API, we take the name the and country Code Creates buttons that has continent names in it --> [Asia] [Afria] [America] [Europe] [Australia]
+//! Example---> when we click on Asia --> Show all countries graph(Fetch from new API "about-corona API") || Be able to select any country in Asia  (in a dropdown menu)
